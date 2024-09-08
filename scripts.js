a3ae8b64145b9e664ce0bdb4865f83e81a2d2923
Srsa <130568954+Steph9145@users.noreply.github.com>
2024-09-08 10:37:33 +0200

    Update
    Mise en ordre des images. Layout de porF2

--- scripts.js
+++ scripts.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentIndex = 0;
     let isPlaying = false; // État du carrousel
     let interval;
-    let isDocumentOpen = false; // Nouveau - Pour gérer l'état du document
-    
+    let isDocumentOpen = false; // État du document
+
     // Sélection des éléments du DOM
     const images = document.querySelectorAll('.carousel img');
     const dots = document.querySelectorAll('.carousel-indicators .dot');
@@ -30,85 +30,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Descriptions détaillées des projets
     const descriptions = [
-        `<p>Analyse des groupements de pays cibles pour exporter nos poulets</p>
-         <ul>
-             <li>Sélection des données pertinentes sur FAO (open source)</li>
-             <li>Préparation, nettoyage et analyse exploratoire des données</li>
-             <li>Classification ascendante hiérarchique / dendrogramme</li>
-             <li>k-means</li>
-             <li>Comparaison des méthodes de clustering puis ACP</li>
-             <li>Choix du cluster et présentation des résultats</li>
-         </ul>`,
-        `<p>Gestion de l'eau en Afrique pour améliorer l'accès</p>
-         <ul>
-             <li>Analyse des besoins en eau potable</li>
-             <li>Conception de systèmes de distribution</li>
-             <li>Évaluation des ressources en eau disponibles</li>
-             <li>Planification des infrastructures nécessaires</li>
-             <li>Coordination avec les autorités locales</li>
-         </ul>`,
-        `<p>Analyse des besoins nutritionnels à l'échelle mondiale</p>
-         <ul>
-             <li>Collecte de données nutritionnelles globales</li>
-             <li>Analyse des carences en micronutriments</li>
-             <li>Modélisation des besoins alimentaires par région</li>
-             <li>Élaboration de recommandations pour les politiques alimentaires</li>
-             <li>Collaboration avec des organisations internationales</li>
-         </ul>`,
-        `<p>Analyse des inégalités de genre en entreprise</p>
-         <ul>
-             <li>Collecte de données sur les écarts de salaire</li>
-             <li>Évaluation des opportunités de carrière</li>
-             <li>Analyse des politiques d'équité en entreprise</li>
-             <li>Proposition de stratégies pour améliorer l'égalité des sexes</li>
-             <li>Suivi des progrès et recommandations</li>
-         </ul>`,
-        `<p>Gestion d'une boutique de vins en ligne</p>
-         <ul>
-             <li>Création d'un catalogue de produits</li>
-             <li>Développement de fonctionnalités de recherche</li>
-             <li>Mise en place d'un système de paiement sécurisé</li>
-             <li>Gestion des stocks et des commandes</li>
-             <li>Analyse des tendances de vente et retour client</li>
-         </ul>`,
-        `<p>Gestion des ventes et inventaire dans une librairie</p>
-         <ul>
-             <li>Suivi des ventes et des inventaires</li>
-             <li>Mise en place d'un système de gestion des commandes</li>
-             <li>Analyse des préférences des clients</li>
-             <li>Optimisation des achats et des stocks</li>
-             <li>Création de promotions ciblées</li>
-         </ul>`,
-        `<p>Détection de faux billets - Machine Learning</p>
-         <ul>
-             <li>Collecte de données sur les caractéristiques des billets</li>
-             <li>Analyse exploratoire (EDA)</li>
-             <li>Régression linéaire : Modèle prédictif pour remplacer valeurs manquantes</li>
-             <li>Analyses et métriques le modèle</li>
-             <li>Création modèle K-means + PCA</li>
-             <li>Régression Logistique Statsmodels et Sklearn</li>
-             <li>Algorithme: Analyse des résultats et ajustements</li>
-             <li>Outil de définition du seuil d’acceptation</li>
-         </ul>`,
-        `<p>Analyse des résultats des élections législatives 2024</p>
-         <ul>
-             <li>Collecte des résultats par circonscription</li>
-             <li>Analyse des tendances de vote</li>
-             <li>Visualisation des résultats par région</li>
-             <li>Comparaison avec les élections précédentes</li>
-             <li>Élaboration de prévisions et recommandations</li>
-         </ul>`
+        "<p>Analyse des groupements de pays cibles pour exporter nos poulets</p><ul><li class='line-spacing'>Sélection des données pertinentes sur FAO (open source)</li><li class='line-spacing'>Préparation, nettoyage et analyse exploratoire des données</li><li class='line-spacing'>Classification ascendante hiérarchique / dendrogramme</li><li class='line-spacing'>k-means</li><li class='line-spacing'>Comparaison des méthodes de clustering puis ACP</li><li class='line-spacing'>Choix du cluster et présentation des résultats</li></ul>",
+        "<p>Gestion de l'eau en Afrique pour améliorer l'accès</p><ul><li class='line-spacing'>Analyse des besoins en eau potable</li><li class='line-spacing'>Conception de systèmes de distribution</li><li class='line-spacing'>Évaluation des ressources en eau disponibles</li><li class='line-spacing'>Planification des infrastructures nécessaires</li><li class='line-spacing'>Coordination avec les autorités locales</li></ul>",
+        "<p>Analyse des besoins nutritionnels à l'échelle mondiale</p><ul><li class='line-spacing'>Collecte de données nutritionnelles globales</li><li class='line-spacing'>Analyse des carences en micronutriments</li><li class='line-spacing'>Modélisation des besoins alimentaires par région</li><li class='line-spacing'>Élaboration de recommandations pour les politiques alimentaires</li><li class='line-spacing'>Collaboration avec des organisations internationales</li></ul>",
+        "<p>Analyse des inégalités de genre en entreprise</p><ul><li class='line-spacing'>Collecte de données sur les écarts de salaire</li><li class='line-spacing'>Évaluation des opportunités de carrière</li><li class='line-spacing'>Analyse des politiques d'équité en entreprise</li><li class='line-spacing'>Proposition de stratégies pour améliorer l'égalité des sexes</li><li class='line-spacing'>Suivi des progrès et recommandations</li></ul>",
+        "<p>Gestion d'une boutique de vins en ligne</p><ul><li class='line-spacing'>Création d'un catalogue de produits</li><li class='line-spacing'>Développement de fonctionnalités de recherche</li><li class='line-spacing'>Mise en place d'un système de paiement sécurisé</li><li class='line-spacing'>Gestion des stocks et des commandes</li><li class='line-spacing'>Analyse des tendances de vente et retour client</li></ul>",
+        "<p>Gestion des ventes et inventaire dans une librairie</p><ul><li class='line-spacing'>Suivi des ventes et des inventaires</li><li class='line-spacing'>Mise en place d'un système de gestion des commandes</li><li class='line-spacing'>Analyse des préférences des clients</li><li class='line-spacing'>Optimisation des achats et des stocks</li><li class='line-spacing'>Création de promotions ciblées</li></ul>",
+        "<p>Détection de faux billets - Machine Learning</p><ul><li class='line-spacing'>Collecte de données sur les caractéristiques des billets</li><li class='line-spacing'>Analyse exploratoire (EDA)</li><li class='line-spacing'>Régression linéaire : Modèle prédictif pour remplacer valeurs manquantes</li><li class='line-spacing'>Analyses et métriques le modèle</li><li class='line-spacing'>Création modèle K-means + PCA</li><li class='line-spacing'>Régression Logistique Statsmodels et Sklearn</li><li class='line-spacing'>Algorithme: Analyse des résultats et ajustements</li><li class='line-spacing'>Outil de définition du seuil d’acceptation</li></ul>",
+        "<p>Analyse des résultats des élections législatives 2024</p><ul><li class='line-spacing'>Collecte des résultats par circonscription</li><li class='line-spacing'>Analyse des tendances de vote</li><li class='line-spacing'>Visualisation des résultats par région</li><li class='line-spacing'>Comparaison avec les élections précédentes</li><li class='line-spacing'>Élaboration de prévisions et recommandations</li></ul>"
     ];
+    
 
     const projectDocuments = [
-        "OCSS_P4_DA-GITHUB.html",  // Document pour le premier projet
-        "OCSS_P6_DA-GITHUB1.html",  // Document pour le deuxième projet
-        "OCSS_P5_DA-GITHUB.html", // Document pour le troisième projet
-        "OCSS_P4_DA-GITHUB.html",     // Document pour le quatrième projet
-        "OCSS_P10_DA_GITHUB.html",        // Document pour le cinquième projet
-        "OCSS_P6_DA-GITHUB1.html",      // Document pour le sixième projet
-        "OCSS_P10_DA_GITHUB.html", // Document pour le septième projet
-        "carte_Legis_1erTour.html"     // Document pour le huitième projet
+        "Projets/OCSS_P4_DA-GITHUB.html",
+        "Projets/OCSS_P6_DA-GITHUB1.html",
+        "Projets/OCSS_P5_DA-GITHUB.html",
+        "Projets/OCSS_P4_DA-GITHUB.html",
+        "Projets/OCSS_P10_DA_GITHUB.html",
+        "Projets/OCSS_P6_DA-GITHUB1.html",
+        "Projets/OCSS_P10_DA_GITHUB.html",
+        "Projets/Results2024_Legis_circonscriptions.html"
     ];
 
     // Logos des outils pour chaque projet
@@ -175,8 +116,8 @@ document.addEventListener('DOMContentLoaded', () => {
         [
             { src: "Images/sklearn-logo.png", alt: "Scikit-learn" },
             { src: "Images/Matplot-logo.png", alt: "Matplotlib" },
-            { src: "Images/seaborn-logo.png", alt: "Matplotlib" },
-            { src: "Images/Numpy-logo.png", alt: "Matplotlib" }
+            { src: "Images/seaborn-logo.png", alt: "Seaborn" },
+            { src: "Images/Numpy-logo.png", alt: "Numpy" }
         ],
         [
             { src: "Images/sql-logo.png", alt: "SQL" },
@@ -198,8 +139,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Met à jour le lien du document
         documentLink.href = projectDocuments[index];
-        // Met à jour l'état du lien
-        toggleDocumentLink();
+        updateDocumentLinkText();
     }
 
     // Met à jour les logos des outils et librairies
@@ -236,12 +176,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Démarre le carrousel automatique
     function startCarousel() {
-        if (!isPlaying) {
+        if (!isPlaying && !isDocumentOpen) {
             interval = setInterval(showNextSlide, 3000);
             isPlaying = true;
             playIcon.style.display = 'none';
             stopIcon.style.display = 'inline';
-            toggleDocumentLink();
+            updateDocumentLinkText(); // Met à jour le texte du lien
         }
     }
 
@@ -252,12 +192,13 @@ document.addEventListener('DOMContentLoaded', () => {
             isPlaying = false;
             playIcon.style.display = 'inline';
             stopIcon.style.display = 'none';
-            toggleDocumentLink();
+            updateDocumentLinkText(); // Met à jour le texte du lien
         }
     }
 
     // Affiche une diapositive spécifique
     function currentSlide(index) {
+        if (isDocumentOpen) return; // Ne fait rien si le document est ouvert
         clearInterval(interval);
         isPlaying = false;
         playIcon.style.display = 'inline';
@@ -266,50 +207,66 @@ document.addEventListener('DOMContentLoaded', () => {
         updateSlide(currentIndex);
     }
 
-    // Fonction pour ouvrir/fermer le document et désactiver/activer les fonctionnalités
+    // Affiche ou masque le conteneur du document du projet
     function toggleDocumentContainer() {
-        if (isDocumentOpen) {
-            // Fermer le document
-            documentContainer.style.visibility = 'hidden';
-            documentContainer.style.opacity = '0';
-            isDocumentOpen = false;
-            // Réactiver le carrousel et les points
-            images.forEach(img => img.style.pointerEvents = 'auto');
-            dots.forEach(dot => dot.style.pointerEvents = 'auto');
-            toggleDocumentLink();
-        } else {
-            // Ouvrir le document
-            document.querySelector('#document-container iframe').src = projectDocuments[currentIndex];
-            documentContainer.style.visibility = 'visible';
-            documentContainer.style.opacity = '1';
+        if (documentContainer.classList.contains('hidden')) {
+            documentContainer.classList.remove('hidden');
             isDocumentOpen = true;
-            // Désactiver le carrousel et les points
-            images.forEach(img => img.style.pointerEvents = 'none');
-            dots.forEach(dot => dot.style.pointerEvents = 'none');
-            stopCarousel(); // Arrêter le carrousel si un document est ouvert
-            toggleDocumentLink();
+            stopCarousel(); // Arrête le carrousel lorsqu'un document est ouvert
+            disableCarouselControls(true); // Désactive les contrôles du carrousel
+            
+            // Calculer la position du document-container avec un espace au bas de la page pour éviter le pied de page
+            const containerOffset = documentContainer.getBoundingClientRect().top + window.scrollY;
+            const offsetAdjustment = 100; // Ajuster cette valeur si nécessaire pour un espace supplémentaire
+            window.scrollTo({
+                top: containerOffset - offsetAdjustment,
+                behavior: 'smooth'
+            });
+        } else {
+            documentContainer.classList.add('hidden');
+            isDocumentOpen = false;
+            startCarousel(); // Relance le carrousel lorsqu'on ferme le document
+            disableCarouselControls(false); // Réactive les contrôles du carrousel
+            // Scroll vers le haut de la page quand le document est fermé
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
         }
+        updateDocumentLinkText(); // Met à jour le texte du lien après modification de isDocumentOpen
     }
-    
-    
 
     // Fonction pour activer ou désactiver le lien et modifier son texte
-    function toggleDocumentLink() {
+    function updateDocumentLinkText() {
         if (isDocumentOpen) {
             documentLink.textContent = 'Fermer le document';
-            documentLink.classList.remove('disabled-link');
+            documentLink.classList.remove('active-link');
+            documentLink.classList.add('default-link');
         } else if (isPlaying) {
-            documentLink.textContent = 'Choisissez un projet'; // Texte quand désactivé
+            documentLink.textContent = 'Choisir un projet';
+            documentLink.classList.remove('active-link');
             documentLink.classList.add('disabled-link');
         } else {
-            documentLink.textContent = 'Voir le document du projet'; // Texte quand activé
+            documentLink.textContent = 'Voir le document du projet';
             documentLink.classList.remove('disabled-link');
+            documentLink.classList.add('active-link');
         }
+    }
+
+    // Fonction pour activer ou désactiver les contrôles du carrousel
+    function disableCarouselControls(disable) {
+        images.forEach(img => {
+            img.style.pointerEvents = disable ? 'none' : 'auto';
+        });
+        dots.forEach(dot => {
+            dot.style.pointerEvents = disable ? 'none' : 'auto';
+        });
     }
 
     // Gestion des clics sur les images pour démarrer/arrêter le carrousel
     images.forEach(img => {
         img.addEventListener('click', () => {
+            if (isDocumentOpen) return; // Ignore les clics si le document est ouvert
             if (isPlaying) {
                 stopCarousel();
             } else {
@@ -327,18 +284,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Gestion du clic sur le lien pour afficher/masquer le document
     documentLink.addEventListener('click', (event) => {
-        if (isPlaying) {
+        if (isDocumentOpen) {
             event.preventDefault(); // Empêche le comportement par défaut du lien
-        } else {
+            toggleDocumentContainer(); // Ferme le document
+            
+        } else if (!isPlaying) {
             event.preventDefault(); // Empêche le comportement par défaut du lien
-            toggleDocumentContainer();
-            // Assure-toi que l'iframe pointe vers le bon document
+            toggleDocumentContainer(); // Ouvre le document
             document.querySelector('#document-container iframe').src = projectDocuments[currentIndex];
         }
     });
-
-    // Met à jour le lien du document à chaque changement de diapositive
-    document.addEventListener('visibilitychange', toggleDocumentLink);
 
     // Initialisation de la diapositive actuelle
     updateSlide(currentIndex);
